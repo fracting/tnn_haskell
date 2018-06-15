2deb06418276e3c3b376e2d4a1400beb928aaee8
Qian Hong <fracting@gmail.com>
2018-06-16 09:56:42 +1000

    WIP.

--- tnn.hs
+++ tnn.hs
@@ -1,11 +1,17 @@
 import Data.Maybe
 
 type SourceWord = Maybe Int
+type SourceSentence = [SourceWord]
+type TargetWord = Maybe Int
+type TargetSentence = [TargetWord]
+type TNNCell = (SourceWord, TargetWord) -> (SourceWord, TargetWord)
 
----tnnCell :: (Integral a, Ord a) => (Maybe a, Maybe a) -> (Maybe a, Maybe a)
+Nothing = Nothing
+
+---tnnCell :: (SourceWord, TargetWord) -> (SourceWord, TargetWord)
 -- tnnCell (x, y) = if x < y then (x, y) else (y, x)
 
-tnnCell :: (Integral a, Ord a) => (Maybe a, Maybe a) -> (Maybe a, Maybe a)
+tnnCell :: TNNCell
 tnnCell (Nothing, Nothing) = (Nothing, Nothing)
 tnnCell (Just 0, Nothing) = (Nothing, Nothing)
 tnnCell (Just x, Nothing) = (Just a, Just b)
@@ -15,14 +21,14 @@ tnnCell (Just x, Just y) = (Just a, Just b)
     where
         (a, b) = divMod (y*16 + x) 10
 
-scanLine :: (Integral a, Ord a) => Maybe a -> [Maybe a] -> ([Maybe a], Maybe a)
+scanLine :: TargetWord -> SourceSentence -> (SourceSentence, TargetWord)
 scanLine input_y [] = ([], input_y)
-scanLine input_y (input_x:input_xs) = (output_x:output_xs, yn)
+scanLine input_y (input_x:input_xs) = (output_x:output_xs, output_y')
     where
-        (output_xs, yn) = scanLine output_y input_xs
+        (output_xs, output_y') = scanLine output_y input_xs
         (output_x, output_y)  = tnnCell (input_x, input_y)
 
-tnn :: (Integral a, Ord a) => [Maybe a] -> [Maybe a]
+tnn :: SourceSentence -> TargetSentence
 tnn xs = case yn of
      Nothing -> []
      Just y  -> (tnn output_xs) ++ [Just y]
