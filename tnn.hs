b112e10bb27b0573834d9504b467b00261656978
Qian Hong <fracting@gmail.com>
2018-06-16 10:34:55 +1000

    Save WIP.

--- tnn.hs
+++ tnn.hs
@@ -1,40 +1,44 @@
 import Data.Maybe
 
 type SourceWord = Maybe Int
-type SourceSentence = [SourceWord]
+type SourceSentence = [Int]
 type TargetWord = Maybe Int
-type TargetSentence = [TargetWord]
+type TargetSentence = [Int]
 type TNNCell = (SourceWord, TargetWord) -> (SourceWord, TargetWord)
 
-Nothing = Nothing
+compareAndSwap :: TNNCell
+compareAndSwap (x, y) = if x < y then (x, y) else (y, x)
 
----tnnCell :: (SourceWord, TargetWord) -> (SourceWord, TargetWord)
--- tnnCell (x, y) = if x < y then (x, y) else (y, x)
-
-tnnCell :: TNNCell
-tnnCell (Nothing, Nothing) = (Nothing, Nothing)
-tnnCell (Just 0, Nothing) = (Nothing, Nothing)
-tnnCell (Just x, Nothing) = (Just a, Just b)
+baseConvCell :: TNNCell
+baseConvCell (Nothing, Nothing) = (Nothing, Nothing)
+baseConvCell (Just 0, Nothing) = (Nothing, Nothing)
+baseConvCell (Just x, Nothing) = (Just a, Just b)
     where 
         (a, b) = divMod x 10
-tnnCell (Just x, Just y) = (Just a, Just b)
+baseConvCell (Just x, Just y) = (Just a, Just b)
     where
         (a, b) = divMod (y*16 + x) 10
 
-scanLine :: TargetWord -> SourceSentence -> (SourceSentence, TargetWord)
-scanLine input_y [] = ([], input_y)
-scanLine input_y (input_x:input_xs) = (output_x:output_xs, output_y')
+scanLine :: TNNCell -> TargetWord -> SourceSentence -> (SourceSentence, TargetWord)
+scanLine tnnCell input_y [] = ([], input_y)
+scanLine tnnCell input_y (input_x:input_xs) = ((maybeToList output_x)++output_xs, output_y')
     where
-        (output_xs, output_y') = scanLine output_y input_xs
-        (output_x, output_y)  = tnnCell (input_x, input_y)
+        (output_xs, output_y') = scanLine tnnCell output_y input_xs
+        (output_x, output_y)  = tnnCell (Just input_x, input_y)
 
-tnn :: SourceSentence -> TargetSentence
-tnn xs = case yn of
+tnn :: TNNCell -> SourceSentence -> TargetSentence
+tnn tnnCell xs = case yn of
      Nothing -> []
-     Just y  -> (tnn output_xs) ++ [Just y]
+     Just y  -> (tnn tnnCell output_xs) ++ [y]
      where
-         (output_xs, yn) = scanLine Nothing xs
+         (output_xs, yn) = scanLine tnnCell Nothing xs
+
+sourceSentence = [11, 4, 2, 3, 1, 10]
 
 main :: IO ()
-main = print $
-    catMaybes $ tnn $ fmap Just [11, 4,  2,  3,  1,  10]
+main = do
+    print $ tnn baseConvCell sourceSentence
+    print $ tnn compareAndSwap sourceSentence
+
+--- base converstion (hex2dec): 0xb4231a(hex) == 11805466(dec)
+--- bubble sort : [11, 4, 2, 3, 1, 10] -> [1,2,3,4,10,11]
